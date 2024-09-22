3b7113bd81c438a983a6099c3029a0c05933eca0
Samuel <samuelzanini.trabalho@gmail.com>
2024-09-22 02:33:01 -0300

    challenge complete 😸✨🎊
    Co-authored-by: Francisco de Oliveira Silva <FrancisWorld@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, MessageMedia } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js'); //add "buttons" if meta allow buttons again
 const qrcode = require('qrcode-terminal');
 
 const client = new Client();
@@ -18,11 +18,25 @@ client.on('message', async (message) => {
         // Declarando variaveis uso global
         const LowerCaseMessage = message.body.toLowerCase()
         const media = MessageMedia.fromFilePath('./chico.jpg')
+        const mediaButton = MessageMedia.fromFilePath('./haha.jpg')
+        
+        /* not working anymore 
+        const messageButton = 
+            new Buttons(
+                'Quer ver algo engraçado?',
+                [
+                    {id: '01', body: 'Sim'},
+                    {id: '02', body: 'Não'},
+                ],
+                'Selecione uma opção',
+                'Obrigado!'
+            );
+        */
 
         if (message.from !== "5512996508094@c.us") {
             return
         }
-
+        
         switch (true) {
             case LowerCaseMessage.includes('oi') || LowerCaseMessage.includes('olá') || LowerCaseMessage.includes('boa tarde') || LowerCaseMessage.includes('boa noite'):
                 message.reply('Olá! Sou o assistente chamado Ivo. Qual o seu nome?')
@@ -30,14 +44,44 @@ client.on('message', async (message) => {
             case LowerCaseMessage.includes('francisco') || LowerCaseMessage.includes('chico') || LowerCaseMessage.includes('francis'):
                 client.sendMessage(message.from, media, { caption: "Olá Francisco!" });
                 break;
+            case LowerCaseMessage.includes('engraçado') || LowerCaseMessage.includes('piada'):
+                client.sendMessage(message.from, mediaButton, { caption: "Claro, aí vai uma piada: Como dizia meu tio mudo..." }); 
+            /* not working anymore 
+            console.log("Tentando mandar botao")
+                try {
+                    await message.dynamicReplyButtons(messageButton);
+                    console.log("Consegui manda o botao :3")
+                }
+                catch (err){
+                    console.log("nao consegui manda o botao, erro: ", err)
+                }
+            */
+                break;
             default:
                 message.reply('Desculpe, no momento só posso responder para o Chico. Tente novamente nas próximas versões!')
                 break;
         }
+
     } catch (e) {
         console.log(e)
 
     }
+
 });
+
+/* not working anymore 
+client.on('message_create', async (message) => {
+    if (message.type === 'buttons_response') {
+        const selectedButton = message.selectedButtonId;
+        const mediaButton = MessageMedia.fromFilePath('./haha.jpg')
+
+        if (selectedButton === '01') {
+            await client.sendMessage(message.from, mediaButton, { caption: `Gato Gojo 😸 hahaha`}); //\u{1F408}
+        } else if (selectedButton === '02') {
+            await client.sendMessage(message.from, 'Certo, quando quiser basta pedir!')
+        }
+    }
+});
+*/
 
 client.initialize();
